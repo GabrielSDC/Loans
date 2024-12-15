c0a45f3935bff9c9978148bbad354d30c67a5806
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2024-12-15 17:38:54 -0300

    unecessary code removed

--- src/main/java/com/back/Emprestimo/control/LoanController.java
+++ src/main/java/com/back/Emprestimo/control/LoanController.java
@@ -2,8 +2,6 @@ package com.back.Emprestimo.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,14 +21,8 @@ public class LoanController {
 		this.loanResponseService = loanResponseService; 
 	}
 	
-	@GetMapping("/loans")
-	public ResponseEntity<String> loans() {
-		return ResponseEntity.ok().body("Aqui deu certo!");
-	}
-	
 	@PostMapping("/customer-loans")
 	public ResponseEntity<LoanResponse> loanResponse(@RequestBody Client client) {
-		System.out.println(client.getName());
 		return ResponseEntity.ok().body(loanResponseService.listLoans(client));
 	}
 }
